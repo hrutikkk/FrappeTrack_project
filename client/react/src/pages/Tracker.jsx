68d313db0a9007c3e84e9bdc9ba82de39ad4c528
hrutikkk <hrutik@unifyxperts.com>
2026-02-02 11:39:50 +0530

    invoked setTimeStatus from Tracker

--- client/react/src/pages/Tracker.jsx
+++ client/react/src/pages/Tracker.jsx
@@ -163,7 +163,7 @@ const Tracker = () => {
       toast.error("Please write description");
       return false;
     }
-
+    window.electronAPI.setTimerStatus(true); // tell main process timer started
     // ✅ validation passed
     start();
     startScreenshots(timeSheetValue);
